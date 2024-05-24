bb4f3961dc6f22d32d30f3795967d047d0351d0c
Ilya Lykov <ilya.lykov.program@gmail.com>
2024-05-24 21:56:07 +0400

    add: create auth middleware for user router flag: NOT TESTED

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const UserRouter = express.Router();
 const UserController = require('./users.controller');
-
+const authenticateToken = require('./users.middleware');
 /* GET home page. */
 
 UserRouter.post('/login', UserController.login);
 UserRouter.post('/register', UserController.register);
-UserRouter.get('/current', UserController.current);
-UserRouter.get('/user/:id', UserController.getUser);
-UserRouter.put('/user/:id', UserController.login);
+UserRouter.get('/current', authenticateToken, UserController.current);
+UserRouter.get('/user/:id', authenticateToken, UserController.getUser);
+UserRouter.put('/user/:id', authenticateToken, UserController.login);
 
 module.exports = UserRouter;
